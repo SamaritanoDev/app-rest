9f72c043f289d1d66b1045079cf96b8e98f7c359
samaritanodev <lesly.samaritano.ayala@gmail.com>
2021-11-28 20:09:11 -0500

    inicio con el formulario - colocanto GestorDetector para que el teclado des/aparesca

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:app_rest/utils/reponsive.dart';
 import 'package:app_rest/widgets/circle.dart';
 import 'package:app_rest/widgets/icon_container.dart';
+import 'package:app_rest/widgets/login_form.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -24,45 +25,47 @@ class _HomePageState extends State<HomePage> {
     Color celeste = const Color(0xffa5dee5);
     double doubleinfinity = double.infinity;
     return Scaffold(
-        body: Container(
-      width: doubleinfinity,
-      height: doubleinfinity,
-      color: Colors.white,
-      child: Stack(alignment: Alignment.center, children: [
-        Positioned(
-          top: -moradosize * 0.4,
-          right: -moradosize * 0.2,
-          child: Circle(
-            size: moradosize, 
-            colors: [morado, lila]),
-        ),
-        Positioned(
-          top: -celestesize * 0.55,
-          left: -celestesize * 0.15,
-          child: Circle(
-            size: celestesize, 
-            colors: [turquesa, celeste]),
-        ),
-        Positioned(
-            top: 130,
-            child: Column(
-              children: [
-                IconContainer(
-                  size: responsive.obtenerAncho(17),
-                ),
-                SizedBox(
-                  height: responsive.obtenerDiagonal(3),
-                ),
-                Text(
-                  "Hola Mundo\nCod3 Girl",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: responsive.obtenerDiagonal(1.6),
+        body: GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Container(
+        width: doubleinfinity,
+        height: doubleinfinity,
+        color: Colors.white,
+        child: Stack(alignment: Alignment.center, children: [
+          Positioned(
+            top: -moradosize * 0.4,
+            right: -moradosize * 0.2,
+            child: Circle(size: moradosize, colors: [morado, lila]),
+          ),
+          Positioned(
+            top: -celestesize * 0.55,
+            left: -celestesize * 0.15,
+            child: Circle(size: celestesize, colors: [turquesa, celeste]),
+          ),
+          Positioned(
+              top: 130,
+              child: Column(
+                children: [
+                  IconContainer(
+                    size: responsive.obtenerAncho(17),
                   ),
-                )
-              ],
-            )),
-      ]),
+                  SizedBox(
+                    height: responsive.obtenerDiagonal(3),
+                  ),
+                  Text(
+                    "Hola Mundo\nCod3 Girl",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: responsive.obtenerDiagonal(1.6),
+                    ),
+                  )
+                ],
+              )),
+          const LoginForm(),
+        ]),
+      ),
     ));
   }
 }
---
+++ lib/widgets/login_form.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({ Key? key }) : super(key: key);
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 30,
+      left: 20,
+      right: 20,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'EMAIL ADDRESS'
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
