7ee44b4490ec72a7f06f59693277a5c53cb938db
samaritanodev <lesly.samaritano.ayala@gmail.com>
2021-11-28 19:11:53 -0500

    creando classe Responsive y adaptando los widgets al dispositivo movil

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_rest/utils/reponsive.dart';
 import 'package:app_rest/widgets/circle.dart';
 import 'package:app_rest/widgets/icon_container.dart';
 import 'package:flutter/material.dart';
@@ -13,10 +14,10 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     //responsive
-    final Size size = MediaQuery.of(context).size;
+    final Responsive responsive = Responsive.of(context);
     //estilos
-    final double moradosize = -(size.width * 0.8);
-    final double celestesize = -(size.width * 0.5);
+    final double moradosize = responsive.obtenerAncho(80);
+    final double celestesize = responsive.obtenerAncho(57);
     Color morado = const Color(0xffcd60da);
     Color lila = const Color(0xffd6a3dc);
     Color turquesa = const Color(0xff75cce8);
@@ -29,31 +30,38 @@ class _HomePageState extends State<HomePage> {
       color: Colors.white,
       child: Stack(alignment: Alignment.center, children: [
         Positioned(
-          top: moradosize * 0.4,
-          right: moradosize * 0.2,
-          child: Circle(size: size.width * 0.8, colors: [morado, lila]),
+          top: -moradosize * 0.4,
+          right: -moradosize * 0.2,
+          child: Circle(
+            size: moradosize, 
+            colors: [morado, lila]),
         ),
         Positioned(
-          top: celestesize * 0.55,
-          left: celestesize * 0.15,
-          child: Circle(size: size.width * 0.57, colors: [turquesa, celeste]),
+          top: -celestesize * 0.55,
+          left: -celestesize * 0.15,
+          child: Circle(
+            size: celestesize, 
+            colors: [turquesa, celeste]),
         ),
         Positioned(
             top: 130,
             child: Column(
               children: [
                 IconContainer(
-                  size: size.width * 0.15,
+                  size: responsive.obtenerAncho(17),
                 ),
-                const SizedBox(
-                  height: 30,
+                SizedBox(
+                  height: responsive.obtenerDiagonal(3),
                 ),
-                const Text("Hola Mundo\nCod3 Girl",
+                Text(
+                  "Hola Mundo\nCod3 Girl",
                   textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: responsive.obtenerDiagonal(1.6),
+                  ),
                 )
               ],
-            )
-        ),
+            )),
       ]),
     ));
   }
