8d66e320ae1c4719576221a2eccb82d92b99878a
samaritanodev <lesly.samaritano.ayala@gmail.com>
2021-11-28 21:16:25 -0500

    fin con el formulario - colocando BorderSize

--- lib/widgets/input_text.dart
+++ lib/widgets/input_text.dart
@@ -3,26 +3,33 @@ import 'package:flutter/material.dart';
 class InputText extends StatelessWidget {
   final String label;
   final TextInputType keyboardType;
-  final bool obscureText;
+  final bool obscureText,borderEnabled;
   const InputText({
     Key? key,
     this.label = '',
     this.keyboardType = TextInputType.text,
     this.obscureText = false,
+    this.borderEnabled = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //Color morado = const Color(0xffcd60da);
     Color turquesa = const Color(0xff75cce8);
     return TextFormField(
       //que tipo de dato es email o pass
       keyboardType: keyboardType,
       obscureText: obscureText,
       decoration: InputDecoration(
-          labelText: label,
-          labelStyle: TextStyle(
-            color: turquesa, 
-            fontWeight: FontWeight.bold)),
+        contentPadding: const EdgeInsets.symmetric(vertical: 10),
+        enabledBorder: borderEnabled? UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: turquesa)
+        ): InputBorder.none,
+        labelText: label,
+        labelStyle: TextStyle(
+          color: turquesa, 
+          fontWeight: FontWeight.bold)),
     );
   }
 }
--- lib/widgets/login_form.dart
+++ lib/widgets/login_form.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:app_rest/widgets/input_text.dart';
 import 'package:flutter/material.dart';
 
@@ -11,25 +12,48 @@ class LoginForm extends StatefulWidget {
 class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
-    //Color morado = const Color(0xffcd60da);
-    //Color lila = const Color(0xffd6a3dc);
-    Color turquesa = const Color(0xff75cce8);
-    //Color celeste = const Color(0xffa5dee5);
 
     return Positioned(
       bottom: 30,
       left: 20,
       right: 20,
       child: Column(
-        children: const [
-          InputText(
+        children: [
+          const InputText(
             keyboardType: TextInputType.emailAddress,
             label: 'EMAIL ADDRESS',
+            borderEnabled: false,
           ),
-          InputText(
-            label: 'PASSWORD',
-            obscureText: true,
-          )
+            Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Color(0xff75cce8),
+                  )
+                )
+              ),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: InputText(
+                      label: 'PASSWORD',
+                      obscureText: true,
+                      borderEnabled: false,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: (){}, 
+                    child: const Text(
+                      "Forgot Password",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xffcd60da),
+                      ),
+                      )
+                    )
+                ]
+              ),
+            ),
         ],
       ),
     );
